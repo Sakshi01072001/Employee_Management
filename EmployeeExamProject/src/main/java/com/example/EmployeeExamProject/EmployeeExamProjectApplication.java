de7773566fe940f5b3940b0ac44ca3e97ff7a3c3
Sakshi01072001 <126318011+Sakshi01072001@users.noreply.github.com>
2023-06-29 14:26:57 +0530

    Add files via upload

---
+++ EmployeeExamProject/src/main/java/com/example/EmployeeExamProject/EmployeeExamProjectApplication.java
@@ -0,0 +1,41 @@
+package com.example.EmployeeExamProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.EmployeeExamProject.Employee;
+import com.example.EmployeeExamProject.EmployeeRepository;
+import com.example.EmployeeExamProject.EmployeeExamProjectApplication;
+
+
+@SpringBootApplication
+public class EmployeeExamProjectApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeExamProjectApplication.class, args);
+	}
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public void run(String... args) throws Exception {
+
+		Employee employee1 = new Employee();
+				employee1.setFirstName("Sakshi");
+				employee1.setLastName("Dongre");
+				employee1.setEmail("sakshi@gmail.com");
+				
+
+		Employee employee2 = new Employee();
+				employee2.setFirstName("Kanchan");
+		employee2.setLastName("Lonarkar");
+		employee2.setEmail("Kanchan@gmail.com");
+
+	
+
+		employeeRepository.save(employee1);
+		employeeRepository.save(employee2);
+		
+	}
+}
